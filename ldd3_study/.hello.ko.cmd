62d1543549215c6620171d7800bd66f6e356b26d
novokrest <novokrest013@gmail.com>
2014-12-15 21:35:56 +0400

    learning linux drivers

---
+++ ldd3_study/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/novokrest/Github/Master/ldd3_study/hello.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-32-generic/scripts/module-common.lds --build-id  -o /home/novokrest/Github/Master/ldd3_study/hello.ko /home/novokrest/Github/Master/ldd3_study/hello.o /home/novokrest/Github/Master/ldd3_study/hello.mod.o
---
+++ ldd3_study/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/novokrest/Github/Master/ldd3_study/hello.ko
+/home/novokrest/Github/Master/ldd3_study/hello.o
