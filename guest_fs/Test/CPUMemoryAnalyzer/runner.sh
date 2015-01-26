5b5c23c75484f388a0a7f7a201236aea1de418e2
novokrest <novokrest013@gmail.com>
2015-01-26 02:06:41 -0800

    add enough ram for test

--- guest_fs/Test/CPUMemoryAnalyzer/runner.sh
+++ guest_fs/Test/CPUMemoryAnalyzer/runner.sh
@@ -10,22 +10,22 @@
 ./CPUMemoryAnalyzer -v -uml -l log_uml.txt
 
 sudo chmod 0000 /dev/kvm
-./CPUMemoryAnalyzer         -l log_gemu.txt
-./CPUMemoryAnalyzer         -l log_gemu.txt
-./CPUMemoryAnalyzer         -l log_gemu.txt
-./CPUMemoryAnalyzer         -l log_gemu.txt
-./CPUMemoryAnalyzer         -l log_gemu.txt
-./CPUMemoryAnalyzer -v      -l log_gemu.txt
-./CPUMemoryAnalyzer -v      -l log_gemu.txt
-./CPUMemoryAnalyzer -v      -l log_gemu.txt
+./CPUMemoryAnalyzer         -l log_qemu.txt
+./CPUMemoryAnalyzer         -l log_qemu.txt
+./CPUMemoryAnalyzer         -l log_qemu.txt
+./CPUMemoryAnalyzer         -l log_qemu.txt
+./CPUMemoryAnalyzer         -l log_qemu.txt
+./CPUMemoryAnalyzer -v      -l log_qemu.txt
+./CPUMemoryAnalyzer -v      -l log_qemu.txt
+./CPUMemoryAnalyzer -v      -l log_qemu.txt
 
 sudo chmod 0666 /dev/kvm
-./CPUMemoryAnalyzer         -l log_gemu_kvm.txt
-./CPUMemoryAnalyzer         -l log_gemu_kvm.txt
-./CPUMemoryAnalyzer         -l log_gemu_kvm.txt
-./CPUMemoryAnalyzer         -l log_gemu_kvm.txt
-./CPUMemoryAnalyzer         -l log_gemu_kvm.txt
-./CPUMemoryAnalyzer -v      -l log_gemu_kvm.txt
-./CPUMemoryAnalyzer -v      -l log_gemu_kvm.txt
-./CPUMemoryAnalyzer -v      -l log_gemu_kvm.txt
+./CPUMemoryAnalyzer         -l log_qemu_kvm.txt
+./CPUMemoryAnalyzer         -l log_qemu_kvm.txt
+./CPUMemoryAnalyzer         -l log_qemu_kvm.txt
+./CPUMemoryAnalyzer         -l log_qemu_kvm.txt
+./CPUMemoryAnalyzer         -l log_qemu_kvm.txt
+./CPUMemoryAnalyzer -v      -l log_qemu_kvm.txt
+./CPUMemoryAnalyzer -v      -l log_qemu_kvm.txt
+./CPUMemoryAnalyzer -v      -l log_qemu_kvm.txt
 
